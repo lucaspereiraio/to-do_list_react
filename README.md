76e03eaedddd7897d491c8f0fa121b0ad7022514
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2023-05-26 13:17:13 -0300

    item-done-state
    now the application is changing the done state of the items instead of only changing the checked state in the checkbox

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a project that create a to-do list for the user, there are a little improvements to be done, but the project already works.
 
-The improments are: change the done state of the items in its root (now is changing only the checked, not the 'item.done') and implement a local-storage to the application.
+Improvement suggestion: implement a local-storage to the application.
--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,16 @@ const App = () => {
     setList(newList);
   };
 
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  };
+
   return (
     <C.Container>
       <C.Area>
@@ -28,7 +38,7 @@ const App = () => {
         <AddArea onEnter={handleAddTask}></AddArea>
         {/* Todo list */}
         {list.map((item, index) => (
-          <ListItem key={index} item={item} />
+          <ListItem key={index} item={item} onChange={handleTaskChange} />
         ))}
       </C.Area>
     </C.Container>
